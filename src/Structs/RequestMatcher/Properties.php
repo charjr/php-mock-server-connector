ef2715bfdce60574a0e3cc0ccff6bc33fb06d666
John Charman <john.charman@imhotek.net>
2025-09-17 15:59:42 +0100

    Make properties private on request properties matcher

--- src/Structs/RequestMatcher/Properties.php
+++ src/Structs/RequestMatcher/Properties.php
@@ -4,28 +4,72 @@ declare(strict_types=1);
 
 namespace Nivseb\PhpMockServerConnector\Structs\RequestMatcher;
 
-use Nivseb\PhpMockServerConnector\Structs\RequestMatcher;
-
-final readonly class Properties implements RequestMatcher
+final class Properties implements \Nivseb\PhpMockServerConnector\Structs\RequestMatcher
 {
     public function __construct(
-        public string $method = '',
-        public string $path = '',
-        public array $pathParameters = [],
-        public array $queryStringParameters = [],
-        public array $headers = [],
-        public array $cookies = [],
-        public array|string $body = '',
+        private string $method = '',
+        private string $path = '',
+        private array $pathParameters = [],
+        private array $queryStringParameters = [],
+        private array $headers = [],
+        private array $cookies = [],
+        private array|string $body = '',
     ) {}
+
+    public function withMethod(string $method): self
+    {
+        $this->method = $method;
+        return $this;
+    }
+
+    public function withPath(string $path): self
+    {
+        $this->path = $path;
+        return $this;
+    }
+
+    /** @param array<string, array|scalar> $parameters */
+    public function withPathParameters(array $parameters): self
+    {
+        $this->pathParameters = $parameters;
+        return $this;
+    }
+
+    /** @param array<string, array|scalar> $parameters */
+    public function withQueryStringParameters(array $parameters): self
+    {
+        $this->queryStringParameters = $parameters;
+        return $this;
+    }
+
+    /** @param array<string, array|scalar> $headers */
+    public function withHeaders(array $headers): self
+    {
+        $this->headers = $headers;
+        return $this;
+    }
+
+    /** @param array<string, scalar> $cookies */
+    public function withCookies(array $cookies): self
+    {
+        $this->cookies = $cookies;
+        return $this;
+    }
+
+    public function withBody(array|string $body): self
+    {
+        $this->body = $body;
+        return $this;
+    }
 
     /**
      * @return array{
      *     method?: string,
      *     path?: string,
-     *     pathParameters?: array,
-     *     queryStringParameters?: array,
-     *     headers?: array,
-     *     cookies?: array,
+     *     pathParameters?: array<array{name:string, value:array|scalar}>,
+     *     queryStringParameters?: array<array{name:string, value:array|scalar}>,
+     *     headers?: array<array{name:string, value:array|scalar}>,
+     *     cookies?: array<array{name:string, value:array|scalar}>,
      *     body?: array|string,
      * }
      */
