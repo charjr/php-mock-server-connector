276689e376bd224f8afce53b421749bc3e738ca4
John Charman <john.charman@imhotek.net>
2025-09-17 15:58:25 +0100

    Allow Expectation to be taken by connector

--- src/Server/Connector.php
+++ src/Server/Connector.php
@@ -10,6 +10,7 @@ use Nivseb\PhpMockServerConnector\Exception\FailResetMockServerException;
 use Nivseb\PhpMockServerConnector\Exception\UnsuccessfulVerificationException;
 use Nivseb\PhpMockServerConnector\Exception\VerificationFailException;
 use Nivseb\PhpMockServerConnector\Expectation\RemoteExpectation;
+use Nivseb\PhpMockServerConnector\Structs\Expectation;
 use Nivseb\PhpMockServerConnector\Structs\MockServerExpectation;
 use Psr\Http\Message\ResponseInterface;
 
@@ -40,7 +41,7 @@ class Connector
     /**
      * @throws FailCreateExpectationException
      */
-    public function applyExpectation(MockServerExpectation $expectation): RemoteExpectation
+    public function applyExpectation(MockServerExpectation|Expectation $expectation): RemoteExpectation
     {
         try {
             $response = $this->client->put(
